3f7c1c90e20039138d326f7a506ecdecfaea2a07
robby <robertapolana@gmail.com>
2015-03-10 17:18:21 +0700

    :sparkles: cleaning

--- public/js/compiled.2.templates.js
+++
@@ -1,3 +0,0 @@
-angular.module("templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("home.html","<h1>HOME</h1>");
-$templateCache.put("ng-app.html","<h1>HELLO</h1>");
-$templateCache.put("profile.html","<h1>PROFILE</h1>");}]);
